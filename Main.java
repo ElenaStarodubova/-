43c03d2c898d48f41ac70de7cb948991e894db9e
ElenaStarodubova <158613200+ElenaStarodubova@users.noreply.github.com>
2024-05-02 00:24:19 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import java.util.Scanner;
 
 public class Main
@@ -7,38 +5,116 @@ public class Main
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         calc.main1();
-
     }
 
 }
 
+
+////////////////////////////////////////////////////////////////////////
 class Calculator {
+
     public static void main1() {
 
         Scanner in = new Scanner(System.in);
-        System.out.print("Введите выражение: ");
+        System.out.print("Введите выражение:");
 
         String input = in.nextLine();
 
-        String[] strs = input.split("(?<=[-+*/])|(?=[-+*/])");
 
-        String str1 = strs[0];
-        String str2 = strs[1];
-        String str3 = strs[2];
+        ///////////////////////////////////////////////////////////////////
+        String[] arrSplit = input.split("\"");
 
 
-        if (str2.equals("-"))  {
 
-            str3 = str3.replaceAll("[^a-zA-Z0-9]", "");
-            String str = str1.replace(str3, "");
-            str = str.replaceAll("[^a-zA-Z0-9]", "");
-            System.out.println(str);
+
+        if (arrSplit[2].equals("+") | arrSplit[2].equals("-")) {
+            Class1 calc_plusminus = new Class1();
+            calc_plusminus.main(arrSplit);
+        }
+
+        //////////////////////////////////////////////////////////////////////////
+        String arrSplit1 = input.replaceAll("\"", "");
+        String[] arrSplit2 = arrSplit1.split("(?<=[\\-+/*])|(?=[\\-+/*])");
+        if (arrSplit2[1].equals("*") | arrSplit2[1].equals("/")) {
+            Class2 calc_proizchastn = new Class2();
+            calc_proizchastn.main(arrSplit2);
+        }
+    }
+}
+
+class Class1 {
+
+    public static void main(String[] arrSplit) {
+        if (arrSplit[2].equals("+")) {
+            if (arrSplit[1].length() > 10|arrSplit[3].length() > 10){
+                System.out.println( "Вы ввели некорректный пример, попробуйте еще раз");
+                throw new IllegalArgumentException();
+            } else {
+                String arrSplitrez = arrSplit[1].concat(arrSplit[3]);
+                System.out.print("Результат: ");
+                System.out.println("\""+arrSplitrez+"\"");
+
+            }
+        }
+        if (arrSplit[2].equals("-"))  {
+            if (arrSplit[1].length() > 10|arrSplit[3].length() > 10){
+                System.out.println( "Вы ввели некорректный пример, попробуйте еще раз");
+                throw new IllegalArgumentException();
+            } else {
+                String arrSplitrez = arrSplit[1].replace(arrSplit[3], "");
+                System.out.print("Результат: ");
+                System.out.println("\""+arrSplitrez+"\"");
+
+            }
+        }
+    }
+}
+// ///////////////////////////////////////////////////////////////////
+class Class2 {
+    public static void main(String[] arrSplit2) {
+        if(arrSplit2[1].equals("/")) {
+            arrSplit2[0] = arrSplit2[0].replaceAll("[^a-zA-Z0-9]", "");
 
         }
-        else if (str2.equals("+")) {
-            String str = str1.concat(str3);
-            str = str.replaceAll("[^a-zA-Z0-9]", "");
-            System.out.println(str);
+        if (arrSplit2[1].equals("*")) {
+            String strNum = arrSplit2[2];
+            int numStr = Integer.parseInt(strNum);
+            if (arrSplit2[0].length() > 10|numStr>10|numStr<0) {
+                System.out.println( "Вы ввели некорректный пример, попробуйте еще раз");
+                throw new IllegalArgumentException();
+            }
+            else   {
+                String veriti = arrSplit2[0].repeat(numStr);
+                if (veriti.length() < 40|veriti.length() == 40){
+                    System.out.print("Результат: ");
+
+                    System.out.println("\""+veriti+"\"");
+
+
+                }
+                if (veriti.length() > 40){
+                    String orgRez = veriti.length() > 40 ? veriti.substring(0, 40) : veriti;
+                    System.out.print("Результат: ");
+                    System.out.println("\""+orgRez+"...\"");
+                }
+
+            }         }
+
+        if (arrSplit2[1].equals("/")) {
+            String strNum = arrSplit2[2];
+            int numStr = Integer.parseInt(strNum);
+            if (arrSplit2[0].length() > 10|numStr>10|numStr<1){
+                System.out.println( "Вы ввели некорректный пример, попробуйте еще раз");
+                throw new IllegalArgumentException();
+            }
+            else    {
+                int y = arrSplit2[0].length()/numStr;
+                System.out.print("Результат: ");
+                int limit = y;
+                String subStr = arrSplit2[0].codePointCount(0, arrSplit2[0].length()) > limit ? arrSplit2[0].substring(0, arrSplit2[0].offsetByCodePoints(0, limit)) : arrSplit2[0];
+                System.out.println("\""+subStr+"\"");
+
+            }
         }
     }
 }
